f977b8203c791d49253f36f20c191dc9932e47d2
Julian Loiacono <jcloiacon@gmail.com>
2022-11-04 00:05:31 -0400

    Create the_ten_commandments.py

---
+++ Ethics/the_ten_commandments.py
@@ -0,0 +1,12 @@
+["You shall have no gods before the Creator", # condemn Yahweh, Allah, Brahma etc
+"Thou shalt not make unto thee any graven image",
+"Thou shalt not take the Creator's name in vain?",
+"Remember the sabbath day, to keep it holy",
+"Honour thy father and thy mother",
+"Thou shalt not murder",
+"Thou shalt not commit adultery",
+"Thou shalt not steal",
+"Thou shalt not bear false witness against thy neighbour",
+"Do not covet your peer's house or spouse, or anything belonging to your neighbor",
+]
+
