604be601430b122e02b3c1003ac9058870b314fe
Julian Loiacono <jcloiacon@gmail.com>
2023-01-11 23:51:34 -0500

    lost America

--- Book Of Julian/Lineage/Material/Migratory.py
+++ Book Of Julian/Lineage/Material/Migratory.py
@@ -7,6 +7,6 @@
  ["There are five races, corresponding to the traditional five continents"],
  {
      "meta": {"type": "lineage", "name": "Migratory Lineage"},
-     "Africa": {"Europe": {}, "Asia": {"America"}, "Australia":{}}, 
+     "Africa": {"Europe": {}, "Asia": {"America": {}}, "Australia":{}}, 
  }
 ]
