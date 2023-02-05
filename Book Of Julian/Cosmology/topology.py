e7e2b1687e4cf3f4b0b8e983cf0035552025b815
Julian Loiacono <jcloiacon@gmail.com>
2023-02-05 11:29:31 -0500

    from old PC

---
+++ Book Of Julian/Cosmology/topology.py
@@ -0,0 +1,4 @@
+{
+    "meta": {"type": "lineage", "name": "God's Universe"},
+    "God's Universe": {"God": {}, "Our Universe": {"Humankind"}},
+}
--- Book Of Julian/Lineage/Material/Migratory.py
+++ Book Of Julian/Lineage/Material/Migratory.py
@@ -1,10 +1,13 @@
 {
     "This is our migratory history, which is the basis of our racial theory": {
-            "meta": {"type": "list"},
-        "300,000 years ago, humankind originated in Africa": {},
-        "150,000 years ago, we expanded into Eurasia": {},
-        "50,000 years ago, we expanded into Australia": {},
-        "15,000 years ago, we expanded into America": {},
+	"meta": {"type": "list"},
+        "300,000 years ago, humankind originated in Africa": {
+            "150,000 years ago, we expanded into Eurasia": {
+                "50,000 years ago, we expanded into Australia": {
+                    "15,000 years ago, we expanded into America": {}
+                }
+            }
+        }
     },
     "There are five races, corresponding to the traditional five continents": {
         "meta": {"type": "lineage", "name": "Migratory Lineage"},
