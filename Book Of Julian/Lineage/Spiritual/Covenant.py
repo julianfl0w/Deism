48543356eb731b9ad0c9e06697175ebffeb1b642
Julian Loiacono <jcloiacon@gmail.com>
2023-02-19 14:21:15 -0500

    Update Covenant.py

--- Book Of Julian/Lineage/Spiritual/Covenant.py
+++ Book Of Julian/Lineage/Spiritual/Covenant.py
@@ -3,10 +3,10 @@
         "meta": {"type": "list"},
         "The First Testament, a covenent of works made with Adam": {},
         "The Second Testament, a covenent of grace made by Jesus": {},
-        "The Third Testament, a covenent of reciprocity made by Julian": {},
+        "The Third Testament, a covenent of love made by Julian": {},
     },
     "Simplified": {
         "meta": {"type": "lineage", "graphParams": {"rankdir": "LR"}},
-        "Works": {"Grace": {"Reciprocity": {}}},
+        "Works": {"Grace": {"Love": {}}},
     },
 }
