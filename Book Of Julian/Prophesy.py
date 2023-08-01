ad6062a0d2ae7f68f3fdacb334dc8d737fc5edd4
Julian Loiacono <jcloiacon@gmail.com>
2023-08-01 14:42:16 -0400

    updates

--- Book Of Julian/Prophesy.py
+++ Book Of Julian/Prophesy.py
@@ -22,11 +22,11 @@
     },
     "The Fourth Temple": {
         "First, all people will be removed from the Dome of the Rock": {},
-        "The gold which now adorns the top will be returned to the Nation of Jordan, or closest heir thereof"
+        "The gold which now adorns the top will be returned to the Nation of Jordan, or closest heir thereof":{},
         "The Third Temple will be destroyed, and a fourth built in its place": {},
         "The Fourth Temple will be a pyramid of clear gold": {},
         "It will be built inside of three years": {},
-        "This event will mark the end of the spiritual age, and the beginning of the metaphysical age": {},
+        "This event will mark the end of the Spiritual Age, and the beginning of the Metaphysical Age": {},
     },
     "The Beatific Vision": {
         "When we see the face of God, this is the Beatific Vision": {},
