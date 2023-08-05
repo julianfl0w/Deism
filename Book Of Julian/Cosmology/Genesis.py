d3efd5993a983f9ba3b0b78dd3139ab6b564fa20
Julian Loiacono <jcloiacon@gmail.com>
2023-08-05 13:46:24 -0400

    Reworks for impact

---
+++ Book Of Julian/Cosmology/Genesis.py
@@ -0,0 +1,14 @@
+{
+    "meta": {"priority": -10000},
+    "Fourteen billion years ago, God created our universe": {},
+    "It started from nothing, then expanded rapidly": {},
+    "And the earth was without form": {},
+    "45 billion years ago, the earth coalesced, in orbit around the sun": {},
+    "37 billion years ago, organized life emerged from the chaos": {},
+    "Many generations passed": {},
+    "300 thousand years ago, humankind originated in Africa": {},
+    "And we expanded throughout the earth": {},
+    "At that time, chaos governed our steps": {},
+    "But we were united under a common God": {},
+    "Now we seek to know God": {},
+}
