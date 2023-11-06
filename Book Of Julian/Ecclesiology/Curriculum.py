60d0789db51dbc43f180fdbab62f47f7a9a1eabd
Julian Loiacono <jcloiacon@gmail.com>
2023-11-05 20:25:25 -0500

    well now it has courses

--- Book Of Julian/Ecclesiology/Curriculum.py
+++ Book Of Julian/Ecclesiology/Curriculum.py
@@ -1,103 +1,91 @@
 {
-    "Observe": {  # Input observations of hard reality
-        "Divine": {
-            "meta": dict(required=1),
-            "Physics": {},  # Emergence downward. Physics is the basis
-            "Chem": {},  # From which arise chem and astro
-            "Astro": {},
-        },
-        "Human": {
-            "meta": dict(required=2),
-            "Body": {
-                "Bio": {
-                    "Stats": {},
-                    "Chem": {},
-                },
-                "DNA": {
-                    "Epigenetics": {},
-                    "Cellular Expression": {},
-                    "Genomics": {},
-                },
-                "Hormone": {},
-            },
-            "Mind": {
-                "Linguistics": {},
-                "Psych": {},
-                "Society": {
-                    "Computationally": {},
-                },
-                "Religion": {},
-                "Government": {},
-                "Histoy": {
-                    "China": {},
-                },
-            },
-        },
+  "Observe": { 
+    "The Cosmos": {
+      "meta": dict(required=1), 
+      "Physics": {"MIT OCW Physics I: Classical Mechanics": "https://ocw.mit.edu/courses/physics/8-01-physics-i-classical-mechanics-fall-1999/"},
+      "Chem": {"Introduction to Chemistry": "https://www.edx.org/course/introduction-to-chemistry"},  
+      "Astro": {"Astronomy: Exploring Time and Space": "https://www.edx.org/course/astronomy-exploring-time-and-space"}
     },
-    "Design": {  # Output design for human
-        "Human": {
-            "Body": {
-                "meta": dict(required=1),
-                "Vision": {},
-                "Senses": {},
-                "Locomotion": {},
-                "Comms": {
-                    "Wired": {},
-                    "Wireless": {},
-                    "Signals": {},
-                    "Secure": {
-                        "Encryption": {},
-                        "Blockchain": {},
-                        "Network": {},
-                    },
-                },
-            },
-            "Mind": {
-                "meta": dict(required=1),
-                "Ops": {
-                    "Scrum Practitioner": {},
-                    "Project Management": {},
-                },
-                "Computer":{
-                    "Cybernet": {},
-                    "Deep NN": {},
-                    "Linear Algebra in GPU": {},
-                },
-                # "Differential Equations": {}, dubious value
-                "Comms": {
-                    "meta": dict(required=2),
-                    "Graphic": {
-                        "Web": {"A Course in HTML/CSS/JS": {}},
-                        "Android": {},
-                        "Game": {
-                            "Unity": {},
-                        },
-                    },
-                    "Cloud Cert": {
-                        "meta": {"required": 1},
-                        "AWS Cloud Practitioner": {},
-                        "AWS Cloud Quest": {},
-                        "GCP Practitioner": {},
-                        "Azure Practitioer": {},
-                    },
-                },
-            },
-        },
-    },
-    "Maintain": {
+    "Human": {
+      "meta": dict(required=2),
+      "Bio": {
+        "Stats": {"Introduction to Probability and Data": "https://www.coursera.org/learn/probability-intro"},
+        "Chem": {"Biochemistry: Life at the Molecular Level": "https://www.edx.org/course/biochemistry-life-at-the-molecular-level"},
+        "Systems": {"Systems Biology": "https://www.coursera.org/learn/systems-biology"}
+      },
+      "Genetics": {
+        "Epigenetics": {"Epigenetic Control of Gene Expression": "https://www.coursera.org/learn/epigenetics"},
+        "Cellular Expression": {"From DNA to Protein: Gene Expression": "https://www.edx.org/course/from-dna-to-protein-gene-expression"},
+        "Genomics": {"Genomic Data Science": "https://www.coursera.org/specializations/genomic-data-science"}
+      },
+      "Linguistics": {"Introduction to Linguistics": "https://www.edx.org/course/introduction-to-linguistics"},
+      "Psych": {"Introduction to Psychology": "https://www.edx.org/course/introduction-to-psychology"},
+      "Society": {
+        "Computationally": {"Model Thinking": "https://www.coursera.org/learn/model-thinking"},
+        "Economics": {"Principles of Microeconomics": "https://www.edx.org/course/principles-of-microeconomics"},
+        "Antropology": {"Introduction to Cultural Anthropology": "https://www.coursera.org/learn/cultural-anthropology"}
+      },
+      "Religion": {"Comparative Religions": "https://www.edx.org/course/comparative-religions"},
+      "Government": {"American Government": "https://www.edx.org/course/american-government"},
+      "History": {
+        "China": {"ChinaX": "https://www.edx.org/school/tsinghuax"}
+      }
+    }
+  },
+  "Design": {
+    "Human": {
+      "meta": dict(required=1),
+      "Vision": {"Computer Vision Basics": "https://www.coursera.org/learn/computer-vision-basics"},
+      "Senses": {"Sensation and Perception": "https://www.edx.org/course/sensation-and-perception"},
+      "Locomotion": {"Biomechanics of Motion": "https://www.edx.org/course/biomechanics-of-motion"},
+      "Networks": {
+        "Wired": {"Computer Networking Basics": "https://www.coursera.org/learn/computer-networking"},
+        "Wireless": {"Wireless Communication": "https://www.edx.org/course/wireless-communication"},
+        "Signals": {"Signals and Systems": "https://www.edx.org/course/signals-and-systems"},
+        "Encryption": {"Cryptography I": "https://www.coursera.org/learn/crypto"},
+        "Blockchain": {"Blockchain Basics": "https://www.edx.org/course/blockchain-basics"}
+      },
+      "Ops": {
+        "Scrum Practitioner": {"Agile Scrum Practitioner": "https://www.coursera.org/professional-certificates/uva-darden-agile-scrum"},
+        "Project Management": {"Project Management Principles and Practices": "https://www.edx.org/course/project-management-principles-practices"}
+      },
+      "Brain": {
+        "Cybernet": {"Control of Mobile Robots": "https://www.coursera.org/learn/mobile-robot"},
+        "Deep NN": {"Neural Networks and Deep Learning": "https://www.coursera.org/learn/neural-networks-deep-learning"},
+        "Linear Algebra in GPU": {"Programming Massively Parallel Processors with CUDA": "https://www.udacity.com/course/programming-massively-parallel-processors-with-cuda--ud977"}
+      },
+      "Comms": {
         "meta": dict(required=2),
-        "Human": {
-            "Mind": {
-                "Therapy": {"Psyc": {}},
-                "Cognitive Behavioral": {},
-                "Book Of Julian": {"meta": dict(required=1)},
-            },
-            "Body": {
-                "Pharma": {},
-                "Imaging": {},
-                "Genes": {},
-                "Blood": {},
-            },
+        "Graphic": {
+          "Web": {"A Course in HTML/CSS/JS": {}},
+          "Android": {"Android App Development": "https://www.udacity.com/course/android-development-for-beginners--ud837"},
+          "Game": {"Unity Game Development": "https://www.coursera.org/specializations/game-development"}
         },
-    },
+        "Cloud Cert": {
+          "meta": {"required": 1},
+          "AWS Cloud Practitioner": {"AWS Cloud Practitioner Essentials": "https://www.aws.training/Details/Curriculum?id=20685"},
+          "AWS Cloud Quest": {"AWS Cloud Quest": "https://run.qwiklabs.com/quests/23"},
+          "GCP Practitioner": {"Google Cloud Digital Leader": "https://www.coursera.org/professional-certificates/google-cloud-digital-leader"},
+          "Azure Practitioer": {"Azure Fundamentals": "https://docs.microsoft.com/en-us/learn/certifications/azure-fundamentals/"}
+        }
+      }
+    }
+  },
+  "Maintain": {
+    "meta": dict(required=2),
+    "Human": {
+      "Mind": {
+        "Therapy": {"Psyc": {}}, 
+        "Cognitive Behavioral": {"Cognitive Behavioral Therapy": "https://www.coursera.org/learn/cbt-depression"},
+        "Book Of Julian": {"meta": dict(required=1)}
+      },
+      "Body": {
+        "Pharma": {"Drug Discovery": "https://www.coursera.org/learn/drug-discovery"},
+        "Imaging": {"Medical Imaging": "https://www.edx.org/course/medical-imaging"},
+        "Genes": {"Genomic Medicine Gets Personal": "https://www.edx.org/course/genomic-medicine-gets-personal"},
+        "Blood": {"Hematology": "https://www.futurelearn.com/courses/hematology"},
+        "Nutrition": {"Stanford Introduction to Food and Health": "https://www.coursera.org/learn/food-and-health"}
+      }
+    }
+  }
 }
