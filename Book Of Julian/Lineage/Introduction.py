7624c8b5eaa095ba8260d9a6482f5ae67ba77296
Julian Loiacono <jcloiacon@gmail.com>
2023-11-26 12:38:50 -0500

    Update Introduction.py

--- Book Of Julian/Lineage/Introduction.py
+++ Book Of Julian/Lineage/Introduction.py
@@ -2,5 +2,5 @@
     "meta": dict(priority=-1000, skipFlare=True),
     "To understand who we are, it is important to trace our lineage": {},
     "Standard Doctrine traces the lineage of humankind and the Deist religion": {},
-    "Five dimensions of lineage are considered: Covenent, Evolutionary, Ideological, Migratory, and Personal": {},
+    "Five dimensions of lineage are considered: Covenant, Evolutionary, Ideological, Migratory, and Personal": {},
 }
