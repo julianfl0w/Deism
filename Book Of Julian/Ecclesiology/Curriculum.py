878e5faac6b9399b86fad8bbe0b6b296950c7b99
Julian Loiacono <jcloiacon@gmail.com>
2023-12-08 15:52:09 -0500

    includes translation, curriculum, and standards

--- Book Of Julian/Ecclesiology/Curriculum.py
+++ Book Of Julian/Ecclesiology/Curriculum.py
@@ -1,88 +1,87 @@
 {
-  "meta":dict(type = "curriculum"),
+  "meta":dict(type = "list"),
   "Observe": { 
     "The Cosmos": {
       "meta": dict(required=1), 
-      "Physics": {"Khan: College Physics 1": {"meta": {"reference": "https://www.khanacademy.org/science/ap-college-physics-1"}},
-      "Chem": {"Khan: Introduction to Chemistry": "https://www.khanacademy.org/science/chemistry/atomic-structure-and-properties/introduction-to-the-atom/v/introduction-to-chemistry"},  
-      "Astro": {"ANUx: Astrophysics: Cosmology": "https://www.edx.org/learn/astrophysics/australian-national-university-astrophysics-cosmology"}
+      "Physics": {"Khan: College Physics 1": {"meta": {"reference": ["https://www.khanacademy.org/science/ap-college-physics-1"]}}},
+      "Chem": {"Khan: Introduction to Chemistry": {"meta": {"reference": ["https://www.khanacademy.org/science/chemistry/atomic-structure-and-properties/introduction-to-the-atom/v/introduction-to-chemistry"]}}},  
+      "Astro": {"ANUx: Astrophysics: Cosmology": {"meta": {"reference": ["https://www.edx.org/learn/astrophysics/australian-national-university-astrophysics-cosmology"]}}}
     },
     "Human": {
       "meta": dict(required=2),
       "Bio": {
-        "Stats": {"Google: The Power of Statistics": "https://www.coursera.org/learn/the-power-of-statistics"},
-        "Chem": {"HarvardX: Principles of Biochemistry": "https://www.edx.org/learn/biochemistry/harvard-university-principles-of-biochemistry"},
+        "Stats": {"Google: The Power of Statistics": {"meta": {"reference": ["https://www.coursera.org/learn/the-power-of-statistics"]}}},
+        "Chem": {"HarvardX: Principles of Biochemistry": {"meta": {"reference": ["https://www.edx.org/learn/biochemistry/harvard-university-principles-of-biochemistry"]}}},
       },
       "Genetics": {
-        "Epigenetics": {"U of Melbourne: Epigenetic Control of Gene Expression": "https://www.coursera.org/learn/epigenetics"},
-        "Genomics": {"Genomic Data Science Specialization": "https://www.coursera.org/specializations/genomic-data-science"}
+        "Epigenetics": {"U of Melbourne: Epigenetic Control of Gene Expression": {"meta": {"reference": ["https://www.coursera.org/learn/epigenetics"]}}},
+        "Genomics": {"Genomic Data Science Specialization": {"meta": {"reference": ["https://www.coursera.org/specializations/genomic-data-science"]}}}
       },
-      "Linguistics": {"Miracles of Human Language: An Introduction to Linguistics": "https://www.coursera.org/learn/human-language"},
-      "Psych": {"St. Margaret\'s Episcopal School: Introduction to Psychology": "https://www.edx.org/course/introduction-to-psychology"},
-      "Psych2": {"Yale: Introduction to Psychology": "https://www.coursera.org/learn/introduction-psychology"},
+      "Linguistics": {"Miracles of Human Language: An Introduction to Linguistics": {"meta": {"reference": ["https://www.coursera.org/learn/human-language"]}}},
+      "Psych": {"St. Margaret\'s Episcopal School: Introduction to Psychology": {"meta": {"reference": ["https://www.edx.org/course/introduction-to-psychology"]}}},
+      "Psych2": {"Yale: Introduction to Psychology": {"meta": {"reference": ["https://www.coursera.org/learn/introduction-psychology"]}}},
       "Society": {
-        "With Models": {"Model Thinking": "https://www.coursera.org/learn/model-thinking"},
-        "Computationally": {"UC Davis: Computational Social Science": "https://www.coursera.org/specializations/computational-social-science-ucdavis"},
-        "Economics": {"UQx: Principles of Microeconomics": "https://www.edx.org/course/principles-of-microeconomics"},
-        "Anthropology": {"UQx: Anthropology of Current World Issues": "https://www.edx.org/learn/anthropology/the-university-of-queensland-anthropology-of-current-world-issues"}
+        "With Models": {"Model Thinking": {"meta": {"reference": ["https://www.coursera.org/learn/model-thinking"]}}},
+        "Computationally": {"UC Davis: Computational Social Science": {"meta": {"reference": ["https://www.coursera.org/specializations/computational-social-science-ucdavis"]}}},
+        "Economics": {"UQx: Principles of Microeconomics": {"meta": {"reference": ["https://www.edx.org/course/principles-of-microeconomics"]}}},
+        "Anthropology": {"UQx: Anthropology of Current World Issues": {"meta": {"reference": ["https://www.edx.org/learn/anthropology/the-university-of-queensland-anthropology-of-current-world-issues"]}}}
       },
-      "Religion": {"A Journey through Western Christianity": "https://www.coursera.org/learn/western-christianity-200-1650"},
-      "Government": {"American Government": "https://www.edx.org/course/american-government"},
+      "Religion": {"A Journey through Western Christianity": {"meta": {"reference": ["https://www.coursera.org/learn/western-christianity-200-1650"]}}},
+      "Government": {"American Government": {"meta": {"reference": ["https://www.edx.org/course/american-government"]}}},
       "History": {
-        "China": {"ChinaX": "https://www.edx.org/school/tsinghuax"}
+        "China": {"ChinaX": {"meta": {"reference": ["https://www.edx.org/school/tsinghuax"]}}}
       }
     }
   },
   "Design": {
     "Human": {
       "meta": dict(required=1),
-      "Vision": {"U at Buffalo: Computer Vision Basics": "https://www.coursera.org/learn/computer-vision-basics"},
-      "Locomotion": {"StanfordOnline: Musculoskeletal Primer for the Non-Orthopedist": "https://www.edx.org/learn/medicine/stanford-university-musculoskeletal-primer-for-the-non-orthopedist"},
+      "Vision": {"U at Buffalo: Computer Vision Basics": {"meta": {"reference": ["https://www.coursera.org/learn/computer-vision-basics"]}}},
+      "Locomotion": {"StanfordOnline: Musculoskeletal Primer for the Non-Orthopedist": {"meta": {"reference": ["https://www.edx.org/learn/medicine/stanford-university-musculoskeletal-primer-for-the-non-orthopedist"]}}},
       "Networks": {
-        "Wired": {"Google: Computer Networking Basics": "https://www.coursera.org/learn/computer-networking"},
-        "Wireless": {"Wireless Communications for Everybody": "https://www.coursera.org/learn/wireless-communications"},
-        "Signals": {"IITBombayX: Signals and Systems, Part 1": "https://www.edx.org/learn/engineering/iitbombay-signals-and-systems-part-1"},
-        "Encryption": {"Stanford: Cryptography I": "https://www.coursera.org/learn/crypto"},
-        "Blockchain": {"IBM: Introduction to Blockchain": "https://www.edx.org/learn/blockchain/ibm-introduction-to-blockchain"}
+        "Wired": {"Google: Computer Networking Basics": {"meta": {"reference": ["https://www.coursera.org/learn/computer-networking"]}}},
+        "Wireless": {"Wireless Communications for Everybody": {"meta": {"reference": ["https://www.coursera.org/learn/wireless-communications"]}}},
+        "Signals": {"IITBombayX: Signals and Systems, Part 1": {"meta": {"reference": ["https://www.edx.org/learn/engineering/iitbombay-signals-and-systems-part-1"]}}},
+        "Encryption": {"Stanford: Cryptography I": {"meta": {"reference": ["https://www.coursera.org/learn/crypto"]}}},
+        "Blockchain": {"IBM: Introduction to Blockchain": {"meta": {"reference": ["https://www.edx.org/learn/blockchain/ibm-introduction-to-blockchain"]}}}
       },
       "Ops": {
-        "Scrum Practitioner": {"Agile Scrum Practitioner": "scrum.org"},
-        "Google Project Management: Professional Certificate": {"https://www.coursera.org/google-certificates/project-management-certificate"}
+        "Scrum Practitioner": {"Agile Scrum Practitioner": {"meta": {"reference": ["scrum.org"]}}},
+        "Google Project Management: Professional Certificate": {"meta": {"reference": ["https://www.coursera.org/google-certificates/project-management-certificate"]}}}
       },
       "Brain": {
-        "Deep NN": {"Neural Networks and Deep Learning": "https://www.coursera.org/learn/neural-networks-deep-learning"},
-        "Linear Algebra in GPU": {"Programming Massively Parallel Processors with CUDA": "https://www.udacity.com/course/programming-massively-parallel-processors-with-cuda--ud977"}
+        "Deep NN": {"Neural Networks and Deep Learning": {"meta": {"reference": ["https://www.coursera.org/learn/neural-networks-deep-learning"]}}},
+        "Linear Algebra in GPU": {"Programming Massively Parallel Processors with CUDA": {"meta": {"reference": ["https://www.udacity.com/course/programming-massively-parallel-processors-with-cuda--ud977"]}}}
       },
       "Comms": {
         "meta": dict(required=2),
         "Graphic": {
-          "Web": {"IBM: Introduction to Web Development with HTML, CSS, JavaScript": "https://www.coursera.org/learn/introduction-to-web-development-with-html-css-javacript"},
-          "Android": {"Meta Android Developer Professional Certificate": "https://www.coursera.org/professional-certificates/meta-android-developer"},
-          "Game": {"Michigan State: Game Design and Development with Unity 2020 Specialization": "https://www.coursera.org/specializations/game-development"}
+          "Web": {"IBM: Introduction to Web Development with HTML, CSS, JavaScript": {"meta": {"reference": ["https://www.coursera.org/learn/introduction-to-web-development-with-html-css-javacript"]}}},
+          "Android": {"Meta Android Developer Professional Certificate": {"meta": {"reference": ["https://www.coursera.org/professional-certificates/meta-android-developer"]}}},
+          "Game": {"Michigan State: Game Design and Development with Unity 2020 Specialization": {"meta": {"reference": ["https://www.coursera.org/specializations/game-development"]}}}
         },
         "Cloud Cert": {
           "meta": {"required": 1},
-          "AWS Cloud Solutions Architect": "https://www.coursera.org/professional-certificates/aws-cloud-solutions-architect",
-          "AWS Cloud Quest": "https://aws.amazon.com/training/digital/aws-cloud-quest/",
-          "Google Cloud Digital Leader": "https://cloud.google.com/learn/certification/cloud-digital-leader",
-          "Azure Fundamentals": "https://docs.microsoft.com/en-us/learn/certifications/azure-fundamentals/"
+          "AWS Cloud Solutions Architect": {"meta": {"reference": ["https://www.coursera.org/professional-certificates/aws-cloud-solutions-architect"]}},
+          "AWS Cloud Quest": {"meta": {"reference": ["https://aws.amazon.com/training/digital/aws-cloud-quest/"]}},
+          "Google Cloud Digital Leader": {"meta": {"reference": ["https://cloud.google.com/learn/certification/cloud-digital-leader"]}},
+          "Azure Fundamentals": {"meta": {"reference": ["https://docs.microsoft.com/en-us/learn/certifications/azure-fundamentals/"]}}
         }
       }
-    }
   },
   "Maintain": {
     "meta": dict(required=2),
     "Human": {
       "Mind": {
-        "Therapy": {"JHU: Psychological First Aid": "https://www.coursera.org/learn/psychological-first-aid"}, 
+        "Therapy": {"JHU: Psychological First Aid": {"meta": {"reference": ["https://www.coursera.org/learn/psychological-first-aid"]}}}, 
         "Book Of Julian": {"meta": dict(required=1)}
       },
       "Body": {
-        "Pharma": {"UC San Diego: Drug Discovery": "https://www.coursera.org/learn/drug-discovery"},
-        "Imaging": {"UQx: Introduction to Biomedical Imaging": "https://www.edx.org/learn/biomedical-engineering/the-university-of-queensland-introduction-to-biomedical-imaging"},
-        "Genes": {"GeorgetownX: Genomic Medicine Gets Personal": "https://www.edx.org/course/genomic-medicine-gets-personal"},
-        "Blood": {"Blood Film Morphology - A Practical Guide": "https://www.coursera.org/learn/blood-film-morphology"},
-        "Nutrition": {"Stanford: Introduction to Food and Health": "https://www.coursera.org/learn/food-and-health"}
+        "Pharma": {"UC San Diego: Drug Discovery": {"meta": {"reference": ["https://www.coursera.org/learn/drug-discovery"]}}},
+        "Imaging": {"UQx: Introduction to Biomedical Imaging": {"meta": {"reference": ["https://www.edx.org/learn/biomedical-engineering/the-university-of-queensland-introduction-to-biomedical-imaging"]}}},
+        "Genes": {"GeorgetownX: Genomic Medicine Gets Personal": {"meta": {"reference": ["https://www.edx.org/course/genomic-medicine-gets-personal"]}}},
+        "Blood": {"Blood Film Morphology - A Practical Guide": {"meta": {"reference": ["https://www.coursera.org/learn/blood-film-morphology"]}}},
+        "Nutrition": {"Stanford: Introduction to Food and Health": {"meta": {"reference": ["https://www.coursera.org/learn/food-and-health"]}}}
       }
     }
   }
