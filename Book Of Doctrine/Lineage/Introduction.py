1c3e0231445e39ed07d7ea2088623a86c3015e07
Julian Loiacono <jcloiacon@gmail.com>
2024-03-10 16:01:24 -0400

    rename Lineage to Origins. include Genesis, and prioritize

--- Book Of Doctrine/Lineage/Introduction.py
+++
@@ -1,6 +0,0 @@
-{
-    "meta": dict(priority=-1000, skipFlare=True),
-    "To understand who we are, it is important to trace our lineage": {},
-    "Standard Doctrine traces the lineage of humankind and the Deist religion": {},
-    "Five dimensions of lineage are considered: Covenant, Evolutionary, Ideological, Migratory, and Personal": {},
-}
